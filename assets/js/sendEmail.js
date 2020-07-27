09325e6dd23bd9eb972d36c3951aa5637f143a98
Alissa Troiano <alissatroianonyc@gmail.com>
2020-07-27 04:15:36 +0000

    Implement emailjs integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,18 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "alissa", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+  emailjs
+    .send("gmail", "alissa", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
 
-        return false;
+  return false;
 }
